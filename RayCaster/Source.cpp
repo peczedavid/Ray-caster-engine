5cc093c98fbf64cf0337e8188155b6d38c0c1686
peczedavid <peczedavid@gmail.com>
2022-06-03 18:04:30 +0200

    Basic setup

---
+++ RayCaster/Source.cpp
@@ -0,0 +1,76 @@
+//#define GLFW_INCLUDE_NONE
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+#include <iostream>
+
+void error_callback(int error, const char* description);
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+void processInput(GLFWwindow* window);
+
+int main() {
+	printf("Program starting");
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwSetErrorCallback(error_callback);
+
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Ray casting engine", NULL, NULL);
+	if (window == NULL) {
+		printf("Failed to create glfw window");
+		glfwTerminate();
+		return -1;
+	}
+	printf("Window created\n");
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		printf("Failed to load glad");
+		return -1;
+	}
+	printf("Glad loaded\n");
+
+
+	printf("Loop start\n");
+	// render loop
+	while (!glfwWindowShouldClose(window)) {
+		// input
+		processInput(window);
+
+		// rendering
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
+		// events/buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	printf("Loop end\n");
+
+
+	glfwTerminate();
+	printf("Program exiting\n");
+	return 0;
+}
+
+void error_callback(int error, const char* description) {
+	fprintf(stderr, "Error: %s\n", description);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
